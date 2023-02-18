242a5eef4b0c17d4d3ea3700a1084770a31dedc5
arwajkhan <55497923+arwajkhan@users.noreply.github.com>
2023-02-18 04:39:03 -0800

    Add files via upload

--- clock.js
+++ clock.js
@@ -1,33 +1,35 @@
-function clock() {
-
-
-    var hours = document.getElementById("hours");
-    var minutes = document.getElementById("minutes");
-    var seconds = document.getElementById("seconds");
-    var am = document.getElementById("am");
-    var time = new Date();
-    var a = time.getHours();
-    var b = time.getMinutes();
-    var c = time.getSeconds();
-
-
-    
-    hours.innerHTML = a;
-    minutes.innerHTML = b;
-    seconds.innerHTML = c;
-    
-
-}
-
-setInterval(clock, 1000);
-
+function currentTime() {
+    let date = new Date(); 
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+    let session = "AM";
+  
+    if(hh == 0){
+        hh = 12;
+    }
+    if(hh > 12){
+        hh = hh - 12;
+        session = "PM";
+     }
+  
+     hh = (hh < 10) ? "0" + hh : hh;
+     mm = (mm < 10) ? "0" + mm : mm;
+     ss = (ss < 10) ? "0" + ss : ss;
+      
+     let time = hh + ":" + mm + ":" + ss + " " + session;
+  
+    document.getElementById("clock").innerText = time; 
+    let t = setTimeout(function(){ currentTime() }, 1000);
+  }
+  currentTime();
 
 
 
 function setTime() {
     var i = document.getElementById("morning").value;
-    var hr = new Date().getTime();
- if(i == hr){
+    var clock = new Date().getTime();
+ if(i == clock){
     document.getElementById("content").innerHTML = "let's have some breakfast";
     document.getElementById("img-container").style.backgroundImage= "url(./images/bf.jpg)";
  }
@@ -40,9 +42,8 @@ function setTime() {
 
 
 function setTime() {
-    var i = document.getElementById("day").value;
-    var hr = new Date().getTime();
- if(i == hr){
+   
+ if(i == clock){
     document.getElementById("content").innerHTML = "let's have some lunch";
     document.getElementById("img-container").style.backgroundImage= "url(./images/lunch.jpg)";
  }
@@ -53,11 +54,11 @@ function setTime() {
  }
 }
 
-
+ 
 function setTime() {
     var i = document.getElementById("night").value;
     var hr = new Date().getTime();
- if(i == hr){
+ if(i == clock){
     document.getElementById("content").innerHTML = "Let's take a nap";
     document.getElementById("img-container").style.backgroundImage= "url(./images/sleep.png)";
  }
@@ -67,6 +68,5 @@ function setTime() {
  
  }
 }
-
 
 
